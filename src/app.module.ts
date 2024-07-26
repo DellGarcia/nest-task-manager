acb8218ecf98fabf48ddc884968c14e1081bbfee
Lucas Del Puerto Garcia <lucas.d.puerto@outlook.com>
2024-07-26 17:59:25 -0300

    Create task and common modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
+import { TaskModule } from './task/task.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    TaskModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
